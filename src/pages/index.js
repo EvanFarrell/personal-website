77dbca126d520b2ede7e8796b36de157f1b1677a
Evan Farrell <evan.farrell0@gmail.com>
2020-10-07 20:17:56 -0400

    update fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,20 +10,20 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div class="container">
       <h1>Evan Farrell</h1>
-      <p class="blurb">Entrepreneur & college dropout.</p>
-      <p class="blurb">
+      <p class="blurb topol subtitle">Entrepreneur & college dropout</p>
+      <p class="blurb topol">
         Cofounded Scholarly, then ScholarMe (naming was a total coincidence,
         believe it or not). I love vintage tech t-shirts, strategy card games, and obsessing over how to maximize deep work.
       </p>
-      <p class="blurb">
+      <p class="blurb topol">
       I believe it's important to work on a few, long, important projects, so I hope this page never gets too big.
       </p>
 
-      <p class="blurb">
-      hmu at firstname at scholarme dot com!
+      <p class="blurb topol">
+      Hit me up at firstname at scholarme dot com
       </p>
 
-      <div class="links">
+      <div class="links topol">
         <a class="link" href="http://blog.evanf.io">
           blog
         </a>
