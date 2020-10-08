a7ee7d487c89fcab4ddbbe71b936d45ed5852cb5
Evan Farrell <evan.farrell0@gmail.com>
2020-10-07 23:37:39 -0400

    google metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Evan Farrell`,
+    description: `Entrepreneur & college dropout`,
+    author: `@__evanf__`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
