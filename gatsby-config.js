3ddce2407d5f0b7ef5d17fab61f8d53b4ce6d36c
Evan Farrell <evan.farrell0@gmail.com>
2020-10-07 23:44:17 -0400

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-transformer-remark',
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     // path: `${__dirname}/src/images`,
-    //     path: `${__dirname}/src/`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        // path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/`,
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
