d9adfa0175171dce80b3da63275fe5b144e4a741
Evan Farrell <evan.farrell0@gmail.com>
2020-12-12 18:53:25 -0500

    add a quote: the person you became

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,52 +15,55 @@ const IndexPage = () => {
     "Remember, you did this to yourself",
     "Gerrard: “But it doesn't do anything!” Hanna: “No—it does nothing.”",
     "Always carry two spears.",
+    "Someone once told me the definition of Hell: The last day you have on earth, the person you became will meet the person you could have become – Anonymous",
   ]
   const quote = items[Math.floor(Math.random() * items.length)]
 
-
   return (
-  <Layout>
-    <SEO title="Home" />
-    <div class="page-wrapper">
-      <div class="container">
-        <h1>Evan Farrell</h1>
-        <p class="blurb topol subtitle">Entrepreneur & college dropout</p>
-        <p class="blurb topol">
-          Cofounded Scholarly, then ScholarMe (naming was a total coincidence,
-          believe it or not). I love vintage tech t-shirts, strategy card games,
-          and obsessing over how to maximize deep work.
-        </p>
-        <p class="blurb topol">
-          I believe it's important to work on a few, long, important projects,
-          so I hope this page never gets too big.
-        </p>
+    <Layout>
+      <SEO title="Home" />
+      <div class="page-wrapper">
+        <div class="container">
+          <h1>Evan Farrell</h1>
+          <p class="blurb topol subtitle">Entrepreneur & college dropout</p>
+          <p class="blurb topol">
+            Cofounded Scholarly, then ScholarMe (naming was a total coincidence,
+            believe it or not). I love vintage tech t-shirts, strategy card
+            games, and obsessing over how to maximize deep work.
+          </p>
+          <p class="blurb topol">
+            I believe it's important to work on a few, long, important projects,
+            so I hope this page never gets too big.
+          </p>
 
-        <p class="blurb topol">Hit me up at firstname at scholarme dot co</p>
+          <p class="blurb topol">Hit me up at firstname at scholarme dot co</p>
 
-        <div class="links topol">
-          <a class="link" href="http://blog.evanf.io">
-            blog
-          </a>
-          <a class="link link-center" href="https://www.scholarme.com">
-            scholarme
-          </a>
-          <a class="link" href="https://www.twitter.com/@__evanf__">
-            twitter
-          </a>
-          <Link class="final-link link" to="/growth-hacks/">
-            growth hacks
-          </Link>
-          <Link class="final-link link" to="/graditude/">
-            graditude
-          </Link>
+          <div class="links topol">
+            <a class="link" href="http://blog.evanf.io">
+              blog
+            </a>
+            <a class="link link-center" href="https://www.scholarme.com">
+              scholarme
+            </a>
+            <a class="link" href="https://www.twitter.com/@__evanf__">
+              twitter
+            </a>
+            <Link class="final-link link" to="/growth-hacks/">
+              growth hacks
+            </Link>
+            <Link class="final-link link" to="/graditude/">
+              graditude
+            </Link>
+          </div>
+        </div>
+        <div class="footer">
+          <p class="blurb topol">
+            <i>{quote}</i>
+          </p>
         </div>
       </div>
-      <div class="footer">
-      <p class="blurb topol"><i>{quote}</i></p>
-      </div>
-    </div>
-  </Layout>
-)}
+    </Layout>
+  )
+}
 
 export default IndexPage
